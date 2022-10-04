0524d58b70e724be1098e3bc003e962233e1bff9
asceznyk <asceznyk@gmail.com>
2022-10-04 12:52:31 +0530

    removed cloud-init from requirements

--- home.py
+++ home.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,5 +8,5 @@ def main_page():
     return render_template('main.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
