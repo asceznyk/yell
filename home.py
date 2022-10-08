fa35622ddbd4fd2b2a98ab389910faff0d120042
asceznyk <asceznyk@gmail.com>
2022-10-08 12:23:05 +0530

    send audio request

--- home.py
+++ home.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
     if request.method == 'POST':
-        print(request)
+        f = request.files['url']
+        f.save('audio.ogg')
     else:
         return render_template('main.html')
 
--- static/audio.js
+++ static/audio.js
@@ -3,7 +3,6 @@ let stop = document.getElementById("stop")
 let audio = document.getElementById("raudio")
 
 if (navigator.mediaDevices) {
-	console.log('getUserMedia supported.');
 
   const constraints = { audio: true };
   let chunks = [];
@@ -15,16 +14,12 @@ if (navigator.mediaDevices) {
 
     record.onclick = () => {
       mediaRecorder.start();
-      console.log(mediaRecorder.state);
-      console.log("recorder started");
       record.style.background = "red";
       record.style.color = "black";
     }
 
     stop.onclick = () => {
       mediaRecorder.stop();
-      console.log(mediaRecorder.state);
-      console.log("recorder stopped");
       record.style.background = "";
       record.style.color = "";
     }
@@ -34,10 +29,8 @@ if (navigator.mediaDevices) {
         mediaRecorder.stop() 
 
         mediaRecorder.ondataavailable = (e) => {
-          console.log('pushing to chunks')
           chunks.push(e.data);
 
-          console.log(chunks.length)
           audio.src = URL.createObjectURL(
             new Blob(chunks, {'type':'audio/ogg; codecs=opus'})
           );
