227a745f454e2e34e478cff5549a08c4affe2596
asceznyk <asceznyk@gmail.com>
2022-10-09 13:26:40 +0530

    sending blob instead of src

--- home.py
+++ home.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
     if request.method == 'POST':
-        audio = request.files.get('audio')
+        #audio = request.files.get('audio')
         return 'audio sent successfully'
     else:
         return render_template('main.html')
