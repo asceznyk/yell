b62b58035ab51cbbb9478975f1e2ad63470013ae
asceznyk <asceznyk@gmail.com>
2022-10-09 13:56:15 +0530

    recognize speech!

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 import os
+import io
 import speech_recognition as sr
 from flask import Flask, render_template, request
 
@@ -8,8 +9,16 @@ model = sr.Recognizer()
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
     if request.method == 'POST':
-        audio = request.files.get('audio')
-        text = model.recognize_google(audio)
+        f = request.files.get('audio')
+        
+        file_obj = io.BytesIO()  
+        file_obj.write(f.read()) 
+        file_obj.seek(0)
+        mic = sr.AudioFile(file_obj)
+        with mic as source:
+            audio = r.record(source)
+            text = model.recognize_google(audio)
+        
         return {'msg':text}
     else:
         return render_template('main.html')
