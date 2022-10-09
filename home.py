5d53c08970b2b3e71c8f14ed729b19e7ca73a036
asceznyk <asceznyk@gmail.com>
2022-10-09 17:14:35 +0530

    test commit..

--- home.py
+++ home.py
@@ -13,8 +13,8 @@ def main_page():
     if request.method == 'POST':
         model = sr.Recognizer()
 
-        file = request.files['file']
-        file.save(os.path.join(app.config['UPLOAD_DIR'], file.filename))
+        #file = request.files['file']
+        #file.save(os.path.join(app.config['UPLOAD_DIR'], file.filename))
 
         #f = open('./file.wav', 'wb')
         #f.write(request.data)
