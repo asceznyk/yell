38fe63f31ca3752126fb27447605bc86d216e072
asceznyk <asceznyk@gmail.com>
2022-10-10 11:40:30 +0530

    test commit..

--- home.py
+++ home.py
@@ -15,7 +15,7 @@ def main_page():
     if request.method == 'POST':
         model = sr.Recognizer()
 
-        fp = os.path.join(app.config["UPLOAD_DIR"], 'sample.wav')
+        #fp = os.path.join(app.config["UPLOAD_DIR"], 'sample.wav')
         audio = request.files['audiof']
 
         #f = open(fp, 'wb')
@@ -23,7 +23,7 @@ def main_page():
         #f.close()
  
         text = ".."
-        with sr.AudioFile(fp) as source: 
+        with sr.AudioFile(audio) as source: 
             print('blech!')
             audio = model.listen(source)
         #text = model.recognize_google(audio)
