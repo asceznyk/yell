6191e380116d04203c3d1a658817f8e4589977ae
asceznyk <asceznyk@gmail.com>
2022-10-12 11:50:08 +0530

    maybe importing numpy changes things?

--- home.py
+++ home.py
@@ -23,7 +23,7 @@ def main_page():
         #fp = os.path.join(app.config["UPLOAD_DIR"], audio.filename) 
         #audio.save(fp)
 
-        wave.open('sample.wav', 'r')
+        wave.open('sample.wav', 'rb')
 
         #rb = wavfile.read('sample.wav')
 
