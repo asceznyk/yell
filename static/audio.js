ed67ad60ede3a7632804de6c68cd2475b35806be
asceznyk <asceznyk@gmail.com>
2023-01-07 16:12:11 +0530

    console.logging..

--- static/audio.js
+++ static/audio.js
@@ -19,20 +19,12 @@ function pauseAudio(audio, btn) {
 
 function customAudioPlayer(audio) {
 	const playBtn = audioPlayer.querySelector(".controls .toggle-play");
-	const timeline = audioPlayer.querySelector(".timeline");
 	const progressBar = audioPlayer.querySelector(".progress");
 	const volumeBtn = audioPlayer.querySelector(".volume-button");
 	const volumeEl = audioPlayer.querySelector(".volume-container .volume"); 
 
-	timeline.addEventListener("click", e => {
-		const timelineWidth = window.getComputedStyle(timeline).width;
-		const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
-		audio.currentTime = timeToSeek;
-	}, false);
-
 	playBtn.addEventListener(
 		"click", () => {
-			audio.src = URL.createObjectURL(new Blob(allChunks))
 			if (audio.paused) {
 				playBtn.classList.remove("play");
 				playBtn.classList.add("pause");
@@ -55,99 +47,89 @@ function customAudioPlayer(audio) {
 		}
 	});
 
-	/*setInterval(() => {
-		progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";	
-		//if (audio.currentTime >= audio.duration) {
-			//pauseAudio(audio, playBtn);
-		//}
-	}, 500);*/
+	setInterval(() => {
+		progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";		
+	}, 500);
 
-	function getTimeCodeFromNum(num) {
-		let seconds = parseInt(num);
-		let minutes = parseInt(seconds / 60);
-		seconds -= minutes * 60;
-		const hours = parseInt(minutes / 60);
-		minutes -= hours * 60;
-
-		if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
-		return `${String(hours).padStart(2, 0)}:${minutes}:${String(
-			seconds % 60
-		).padStart(2, 0)}`;
+	audio.onended = (e) => {
+		alert("done playing audio!");
+		audio.src = URL.createObjectURL(new Blob(allChunks));
 	}
 
 	return playBtn;
 }
 
-//function liveAudioSpeechRecognition() {
-let recordBtn = document.getElementById("record");
-let stopBtn = document.getElementById("stop");
+function liveAudioSpeechRecognition(audio) {
+	let recordBtn = document.getElementById("record");
+	let stopBtn = document.getElementById("stop");
 
-if (navigator.mediaDevices) {
-	navigator.mediaDevices.getUserMedia({audio: true})
-	.then((stream) => {
-		const mediaRecorder = new MediaRecorder(stream, {
-			mimeType: 'audio/webm; codecs=opus'
-		})
+	if (navigator.mediaDevices) {
+		navigator.mediaDevices.getUserMedia({audio: true})
+		.then((stream) => {
+			const mediaRecorder = new MediaRecorder(stream, {
+				mimeType: 'audio/webm; codecs=opus'
+			})
 
-		recordBtn.onclick = () => {
-			allChunks = [];
-			allTexts = [];
-			audioTag.src = "";
-			transcriptDiv.innerHTML = `<span>Re-annotating..</span>`;
-			console.log('start recording');
-			stopped = 0;
-			mediaRecorder.start();
-			recordBtn.style.background = "red";
-			record.style.color = "white";
-		}
-
-		stopBtn.onclick = (e) => {
-			console.log('stop recording');
-			stopped = 1;
-			mediaRecorder.stop();
-			record.style.background = "";
-			record.style.color = "black";	
-		}
-
-		setInterval(function() { 
-			if(mediaRecorder.state == "recording") {
-				mediaRecorder.stop();	
-			}	
-		}, 2000);
-
-		mediaRecorder.ondataavailable = (e) => {
-			console.log('ondataavailable1 fired!');
-			allChunks.push(e.data);
-			if (!stopped) {
-				let fd = new FormData();
-				fd.append('audio_blob', new Blob([e.data]), `${guid}.webm`)
-				fd.append('browser_id', guid)
-
-				console.log('resuming media and sending audio request..');
-
+			recordBtn.onclick = () => {
+				allChunks = [];
+				allTexts = [];
+				audio.src = "";
+				transcriptDiv.innerHTML = `<span>Re-annotating..</span>`;
+				console.log('start recording');
+				stopped = 0;
 				mediaRecorder.start();
-				fetch("/", {
-					method: "post",
-					body: fd,  
-				})
-				.then((response) => response.json())
-				.then((data) => {
-					text = data.msg;
-					if (!text.includes('err_msg')) {
-						allTexts.push(text);
-						transcriptDiv.innerHTML = `<span>${allTexts.join(' ')} </span>` 
-					} 
-				}); 
-			} 	
-		}
-	})
-	.catch((err) => {
-		console.error(`The following error occurred: ${err}`);
-	})
-}
-//}
+				recordBtn.style.background = "red";
+				record.style.color = "white";
+			}
 
-//liveAudioSpeechRecognition();
+			stopBtn.onclick = (e) => {
+				console.log('stop recording');
+				stopped = 1;
+				mediaRecorder.stop();
+				record.style.background = "";
+				record.style.color = "black";	
+				audio.src = URL.createObjectURL(new Blob(allChunks))
+			}
+
+			setInterval(function() { 
+				if(mediaRecorder.state == "recording") {
+					mediaRecorder.stop();	
+				}	
+			}, 2000);
+
+			mediaRecorder.ondataavailable = (e) => {
+				console.log('ondataavailable1 fired!');
+				allChunks.push(e.data);
+				if (!stopped) {
+					let fd = new FormData();
+					fd.append('audio_blob', new Blob([e.data]), `${guid}.webm`)
+					fd.append('browser_id', guid)
+
+					console.log('resuming media and sending audio request..');
+
+					mediaRecorder.start();
+					fetch("/", {
+						method: "post",
+						body: fd,  
+					})
+					.then((response) => response.json())
+					.then((data) => {
+						text = data.msg;
+						if (!text.includes('err_msg')) {
+							allTexts.push(text);
+							transcriptDiv.innerHTML = `<span>${allTexts.join(' ')} </span>` 
+						} 
+					}); 
+				} 	
+			}
+		})
+		.catch((err) => {
+			console.error(`The following error occurred: ${err}`);
+		})
+	}
+}
+
+liveAudioSpeechRecognition(audioTag);
 
 
 
